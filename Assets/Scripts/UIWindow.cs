cc64360fc887be3d55760537043ec119bf13a2c7
elichaosx <elicelaena2004@outlook.com>
2026-09-18 21:26:19 -0600

    Creación de Scrip

---
+++ Assets/Scripts/UIWindow.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class UIWindow : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    public virtual void Show()
+    {
+        
+    }
+    
+    public virtual void Hide()
+    {
+            
+    }
+}
